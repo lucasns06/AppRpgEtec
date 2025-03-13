f56688a6ff9e52a782e0f4e195bd8ac6633f906c
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-03-13 10:18:38 -0300

    upload

---
+++ ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Usuarios;
+using AppRpgEtec.Views.Usuarios;
+
+namespace AppRpgEtec.ViewModels.Usuarios
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        private UsuarioService uService;
+        public ICommand AutenticarCommand { get; set; }
+        public ICommand RegistrarCommand { get; set; }
+        public ICommand DirecionarCadastroCommand { get; set; }
+        public UsuarioViewModel()
+        {
+            uService = new UsuarioService();
+            InicializarCommands();
+        }
+        public void InicializarCommands()
+        {
+            AutenticarCommand = new Command(async () => await AutenticarUsuario());
+            RegistrarCommand = new Command(async () => await RegistrarUsuario());
+            DirecionarCadastroCommand = new Command(async () => await DirecionarParaCadastro());
+        }
+        #region Métodos
+        public async Task RegistrarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = Login;
+                u.PasswordString = Senha;
+
+                Usuario uRegistrado =  await uService.PostRegistrarUsuarioAsync(u);
+
+                if(uRegistrado.Id != 0)
+                {
+                    string mensagem = $"Usuário id: {uRegistrado.Id} registrado com sucesso";
+                    await Application.Current.MainPage.DisplayAlert("Informação", mensagem, "OK");
+
+                    await Application.Current.MainPage.Navigation.PopAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                       .DisplayAlert("Informação", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        #endregion
+        #region AtributosPropriedades
+
+
+        private string login = string.Empty;
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged();
+            }
+        }
+        private string senha = string.Empty;
+        public string Senha
+        {
+            get { return senha; }
+            set
+            {
+                senha = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        public async Task AutenticarUsuario()
+        {
+            try
+            {
+                Usuario u = new Usuario();
+                u.Username = login;
+                u.PasswordString = senha;
+
+                Usuario uAutenticado = await uService.PostAutenticarUsuarioAsync(u);
+
+                if (!string.IsNullOrEmpty(uAutenticado.Token)) 
+                {
+                    string mensagem = $"Bem-vindo(a) {uAutenticado.Username}.";
+
+                    Preferences.Set("UsuarioId", uAutenticado.Id);
+                    Preferences.Set("UsuarioUsername", uAutenticado.Username);
+                    Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+                    Preferences.Set("UsuarioToken", uAutenticado.Token);
+
+                    await Application.Current.MainPage.DisplayAlert("Informação", mensagem, "Ok");
+
+                    Application.Current.MainPage = new MainPage();
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Informação", "Dados Incorretos :(", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                       .DisplayAlert("Informação", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        public async Task DirecionarParaCadastro()
+        {
+            try
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new CadastroView());
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Informação", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
